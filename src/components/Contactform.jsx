1cae98e9b66abed75ae7ebf4e98822dac98c5251
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-14 20:44:38 +0000

    amended the contact form code to use contact form

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -14,17 +14,17 @@ const ContactForm = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" {...register('name', { required: true })} />
+          <input type="text" name="name" id="name" {...register('name', { required: true })} autoComplete="name" />
           {errors.name && <span className="error-message">This field is required</span>}
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" {...register('email', { required: true })} />
+          <input type="email" name="email" id="email" {...register('email', { required: true })} autoComplete="email" />
           {errors.email && <span className="error-message">This field is required</span>}
         </div>
         <div>
           <label htmlFor="question">Question</label>
-          <textarea name="question" id="question" {...register('question')} />
+          <textarea name="question" id="question" {...register('question')} autoComplete="off" />
         </div>
         <button type="submit">Submit</button>
       </form>
@@ -33,3 +33,4 @@ const ContactForm = () => {
 };
 
 export default ContactForm;
+
