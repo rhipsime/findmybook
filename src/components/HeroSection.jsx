b891743eb12136ce4c576fc04ee0a5f7fdae591b
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-03-14 11:21:30 +0000

    updated herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import heroImage from '../assets/heroImage.png'
 const HeroSection = () => {
   return (
     <div>
-    <img src={heroImage} alt='hero image' className='h-full w-full flex justify-center'></img>
+    <img src={heroImage} alt='hero image' className='h-1/2 w-full flex justify-center'></img>
     </div>
   )
 }
