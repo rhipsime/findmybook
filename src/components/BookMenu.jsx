4b8adb845fab125acc07604aa1b22f67cbcefb73
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-03-15 19:25:45 +0000

    updated some styles for the bookmenu

--- src/components/BookMenu.jsx
+++ src/components/BookMenu.jsx
@@ -6,15 +6,16 @@ import fallback from '../assets/fallback.png'
 const BookMenu = () => {
   const [books, setBooks] = useState([]);
   const [term, setTerm] = useState('');
-
   useEffect(() => {
-    axios.get('https://www.googleapis.com/books/v1/volumes?q=' + term + '&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU' + '&maxResults=40')
-      .then(response => {
+    const fetchBooks = async () => {
+      try {
+        const response = await axios.get('https://www.googleapis.com/books/v1/volumes?q=' + term + '&key=AIzaSyDaJaU2_8QkMUfXJuAnrBOz_O5N8D_hvw4' + '&maxResults=40')
         setBooks(response.data.items);
-      })
-      .catch(error => {
-        console.log('Error fetching data: ', error);
-      });
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+    fetchBooks();
   }, [term]);
 
   const openAmazonPage = (bookauthors) => {
@@ -22,7 +23,7 @@ const BookMenu = () => {
     window.open(amazonUrl, '_blank');
   };
 
-  return (
+    return (
     <div className="container mx-auto p-4">
       <input
         type="text"
@@ -30,31 +31,27 @@ const BookMenu = () => {
         onChange={(e) => setTerm(e.target.value)}
         className="w-full p-2 border-4 border-orange-500 rounded-md"
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 mt-4 shadow-lg">
         {books.map((book) => (
-          <div key={book.id} className="bg-white shadow-md rounded-md p-4">
+          <div key={book.id} className="bg-white shadow-md rounded-md p-4 flex flex-col items-center">
             <img
               src={book.volumeInfo.imageLinks?.thumbnail || fallback}
               alt={book.volumeInfo.title}
-              className="w-full h-48 object-cover rounded-md cursor-pointer"
+              className='object-scale-down w-48 h-48 cursor-pointer rounded-md'
               onClick={() => openAmazonPage(book.volumeInfo.authors)}
               onError={(e) => {
                 e.target.src = fallback;
               }}
             />
-
             <div className="flex justify-center">
               <button className="mt-2 bg-orange-500 hover:bg-blue-400 text-black font-bold py-2 px-4 rounded">
                 Add to Favourites
               </button>
             </div>
-
-
           </div>
         ))}
       </div>
     </div>
   );
 };
-
 export default BookMenu;
